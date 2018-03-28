be0c8790579b4ce05f0d4f9bd0ecb56dcad3de63
siburuxue <siburuxue@sina.com>
2018-03-28 13:29:09 +0800

    修改跳转方式

--- auth_callback.php
+++ auth_callback.php
@@ -9,8 +9,9 @@ if($_REQUEST['state'] == 'code'){
     $curl = curl_init();
     //设置抓取的url
     curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl,CURLOPT_HEADER,['Accept:application/json']);
     //设置头文件的信息作为数据流输出
-    curl_setopt($curl, CURLOPT_HEADER, 1);
+    curl_setopt($curl, CURLOPT_HEADER, 0);
     //设置获取的信息以文件流的形式返回，而不是直接输出。
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     //执行命令
@@ -18,5 +19,5 @@ if($_REQUEST['state'] == 'code'){
     //关闭URL请求
     curl_close($curl);
     //显示获得的数据
-    print_r($data);
+    echo $data;
 }
