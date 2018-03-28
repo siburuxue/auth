9d6c603c06e2f1d6fa5b8d36a3b5c745a0d42b4c
siburuxue <siburuxue@sina.com>
2018-03-28 11:09:33 +0800

    修改跳转方式

--- auth_callback.php
+++ auth_callback.php
@@ -2,7 +2,7 @@
 $config = require_once("config.php");
 if($_REQUEST['state'] == 'code'){
     $code = $_REQUEST['code'];
-    $url = "POST https://github.com/login/oauth/access_token";
+    $url = "https://github.com/login/oauth/access_token";
     $redirect_uri = "http://auth.siburuxue.com/auth_callback.php";
     $url = $url."?client_id=".$config['github']['app_id']."&client_secret=".$config['github']['app_secret']."&code=".$code."&redirect_uri=".urlencode($redirect_uri)."&state=token";
     header("Location:".$url);
