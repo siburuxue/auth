b0210346f94b37b488a79bac32e9fe225048c0b3
siburuxue <siburuxue@sina.com>
2018-03-28 14:02:26 +0800

    修改跳转方式

--- auth_callback.php
+++ auth_callback.php
@@ -10,7 +10,7 @@ if($_REQUEST['state'] == 'code'){
     $data = json_decode($data,true);
     $token = $data['token'];
     $user_info = "https://api.github.com/user?access_token=".$token;
-    $header = ['User-Agent:qlxdj','Accept:application/vnd.github.jean-grey-preview+json'];
+    $header = ['User-Agent:siburuxue','Accept:application/vnd.github.jean-grey-preview+json'];
     $rs = curl_get($user_info,$header);
     echo $rs;
 }
