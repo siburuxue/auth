191c77ec75c7e7c7811cf02ed120c1e02f9f5963
siburuxue <siburuxue@sina.com>
2018-03-28 10:50:53 +0800

    first commit

---
+++ auth.php
@@ -0,0 +1,9 @@
+<?php
+$config = require_once("config.php");
+$url = "https://github.com/login/oauth/authorize";
+$auth_redirect = "http://auth.siburuxue.com/auth_callback.php";
+$scope = "scope";
+$state = time();
+$allow_signup = true;
+$url = $url."?client_id=".$config['github']['app_id']."&redirect_uri=".urlencode($auth_redirect)."&scope=".$scope."&state=".$state."&allow_signup=".$allow_signup;
+header($url);
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'github' => [
+        'app_id' => "d76c6418b1fe6fb7a67e",
+        'app_secret' => "558fed46acf44438592ba711a03d487edfbcd794",
+    ],
+];
