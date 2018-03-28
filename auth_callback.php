1e3c5f5cf9f5e6fa44682fa5a70cd71ae3fd8cf0
siburuxue <siburuxue@sina.com>
2018-03-28 11:22:55 +0800

    修改跳转方式

--- auth_callback.php
+++ auth_callback.php
@@ -5,9 +5,31 @@ if($_REQUEST['state'] == 'code'){
     $code = $_REQUEST['code'];
     $url = "https://github.com/login/oauth/access_token";
     $redirect_uri = "http://auth.siburuxue.com/auth_callback.php";
-    $url = $url."?client_id=".$config['github']['app_id']."&client_secret=".$config['github']['app_secret']."&code=".$code."&redirect_uri=".urlencode($redirect_uri)."&state=token";
-    header("Accept:application/json");
-    header("Location:".$url);
+    $headers = [
+        'Accept: application/json'
+    ];
+    $data = [
+        'client_id' => $config['github']['app_id'],
+        'client_secret' => $config['github']['app_secret'],
+        'code' => $code,
+        'redirect_uri' => urlencode($redirect_uri),
+        'state' => "token",
+    ];
+    $rs = postCurl($url,$data,$headers);
+    echo $rs;
 }else if($_REQUEST['state'] == 'token'){
 
+}
+function postCurl($url,$post_data,$headers=[]){
+    $ch = curl_init();
+    if(!empty($headers)){
+        curl_setopt ( $ch, CURLOPT_HTTPHEADER, $headers);
+    }
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
 }
