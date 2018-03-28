43440cff779f7a7e38d47f963edc1f648ffc4f0e
siburuxue <siburuxue@sina.com>
2018-03-28 14:19:38 +0800

    修改跳转方式

--- auth_callback.php
+++ auth_callback.php
@@ -9,10 +9,11 @@ if($_REQUEST['state'] == 'code'){
     $data = curl_get($url,$header);
     $data = json_decode($data,true);
     $token = $data['token'];
-    $user_info = "https://api.github.com/user?access_token=".$token;
-    $header = ['User-Agent:siburuxue'];
-    $rs = curl_get($user_info,$header);
-    echo $rs;
+    echo $token;
+    //$user_info = "https://api.github.com/user?access_token=".$token;
+    //$header = ['User-Agent:siburuxue'];
+    //$rs = curl_get($user_info,$header);
+    //echo $rs;
 }
 function curl_get($url,$header=[]){
     $curl = curl_init();
