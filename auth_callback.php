0f7598f81627aceb89ca8226a02117c473938da9
siburuxue <siburuxue@sina.com>
2018-03-28 14:28:44 +0800

    修改跳转方式

--- auth_callback.php
+++ auth_callback.php
@@ -1,19 +1,17 @@
 <?php
 $config = require_once("config.php");
-if($_REQUEST['state'] == 'code'){
-    $code = $_REQUEST['code'];
-    $url = "https://github.com/login/oauth/access_token";
-    $redirect_uri = "http://auth.siburuxue.com/auth_callback.php";
-    $url = $url."?client_id=".$config['github']['app_id']."&client_secret=".$config['github']['app_secret']."&code=".$code."&redirect_uri=".urlencode($redirect_uri)."&state=token";
-    $header = ['Accept:application/json'];
-    $data = curl_get($url,$header);
-    $data = json_decode($data,true);
-    $token = $data['access_token'];
-    $user_info = "https://api.github.com/user?access_token=".$token;
-    $header = ['User-Agent:siburuxue'];
-    $rs = curl_get($user_info,$header);
-    echo $rs;
-}
+$code = $_REQUEST['code'];
+$url = "https://github.com/login/oauth/access_token";
+$redirect_uri = "http://auth.siburuxue.com/auth_callback.php";
+$url = $url."?client_id=".$config['github']['app_id']."&client_secret=".$config['github']['app_secret']."&code=".$code."&redirect_uri=".urlencode($redirect_uri)."&state=token";
+$header = ['Accept:application/json'];
+$data = curl_get($url,$header);
+$data = json_decode($data,true);
+$token = $data['access_token'];
+$user_info = "https://api.github.com/user?access_token=".$token;
+$header = ['User-Agent:siburuxue'];
+$rs = curl_get($user_info,$header);
+echo $rs;
 function curl_get($url,$header=[]){
     $curl = curl_init();
     if(!empty($header)){
