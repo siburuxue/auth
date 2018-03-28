42cafe8e65ef26a4755701fdac2f0af02feed883
siburuxue <siburuxue@sina.com>
2018-03-28 13:26:38 +0800

    修改跳转方式

--- auth_callback.php
+++ auth_callback.php
@@ -1,25 +1,22 @@
 <?php
 $config = require_once("config.php");
-$response = file_get_contents("php://input");
-error_log($response."\r\n",3,"response.log");
 if($_REQUEST['state'] == 'code'){
     $code = $_REQUEST['code'];
     $url = "https://github.com/login/oauth/access_token";
     $redirect_uri = "http://auth.siburuxue.com/auth_callback.php";
     $url = $url."?client_id=".$config['github']['app_id']."&client_secret=".$config['github']['app_secret']."&code=".$code."&redirect_uri=".urlencode($redirect_uri)."&state=token";
-    header("Accept:application/json");
-    header("Location:".$url);
-}
-function postCurl($url,$post_data,$headers=[]){
-    $ch = curl_init();
-    if(!empty($headers)){
-        curl_setopt ( $ch, CURLOPT_HTTPHEADER, $headers);
-    }
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
-    $output = curl_exec($ch);
-    curl_close($ch);
-    return $output;
+    //初始化
+    $curl = curl_init();
+    //设置抓取的url
+    curl_setopt($curl, CURLOPT_URL, $url);
+    //设置头文件的信息作为数据流输出
+    curl_setopt($curl, CURLOPT_HEADER, 1);
+    //设置获取的信息以文件流的形式返回，而不是直接输出。
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    //执行命令
+    $data = curl_exec($curl);
+    //关闭URL请求
+    curl_close($curl);
+    //显示获得的数据
+    print_r($data);
 }
