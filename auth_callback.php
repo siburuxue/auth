83b4a3687c61ec0668ffc2d779e0fda4120200ca
siburuxue <siburuxue@sina.com>
2018-03-28 11:26:36 +0800

    修改跳转方式

--- auth_callback.php
+++ auth_callback.php
@@ -28,7 +28,7 @@ function postCurl($url,$post_data,$headers=[]){
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
     $output = curl_exec($ch);
     curl_close($ch);
     return $output;
