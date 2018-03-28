e7ca379a956f4c2d959c2c27b7ce5da17ae65f62
siburuxue <siburuxue@sina.com>
2018-03-28 11:08:25 +0800

    修改跳转方式

--- auth.php
+++ auth.php
@@ -3,7 +3,7 @@ $config = require_once("config.php");
 $url = "https://github.com/login/oauth/authorize";
 $auth_redirect = "http://auth.siburuxue.com/auth_callback.php";
 $scope = "scope";
-$state = time();
+$state = 'code';
 $allow_signup = true;
 $url = $url."?client_id=".$config['github']['app_id']."&redirect_uri=".urlencode($auth_redirect)."&scope=".$scope."&state=".$state."&allow_signup=".$allow_signup;
 header("Location: ".$url);
--- auth_callback.php
+++ auth_callback.php
@@ -1,2 +1,11 @@
 <?php
-echo json_encode($_REQUEST);
+$config = require_once("config.php");
+if($_REQUEST['state'] == 'code'){
+    $code = $_REQUEST['code'];
+    $url = "POST https://github.com/login/oauth/access_token";
+    $redirect_uri = "http://auth.siburuxue.com/auth_callback.php";
+    $url = $url."?client_id=".$config['github']['app_id']."&client_secret=".$config['github']['app_secret']."&code=".$code."&redirect_uri=".urlencode($redirect_uri)."&state=token";
+    header("Location:".$url);
+}else if($_REQUEST['state'] == 'token'){
+    echo json_encode($_REQUEST);
+}
