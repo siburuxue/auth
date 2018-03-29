b47c4cb6e55f5ac322718a900969e5b3270cfd66
siburuxue <siburuxue@sina.com>
2018-03-29 23:01:21 +0800

    修改为工厂模式

--- Factory.php
+++ Factory.php
@@ -16,13 +16,27 @@ class Factory
 {
     private $type;
 
+    private $obj;
+
     public function __construct($type)
     {
         $this->type = $type;
     }
 
+    public function __call($name,$args){
+        $this->execute($name,$args);
+    }
+
     public function create(){
-        echo $this->type;
-        return new $this->type;
+        $this->obj = new \ReflectionMethod($this->type);
+        return $this;
+    }
+
+    public function execute($name,$args){
+        if(empty($args)){
+            $this->obj->invoke($name);
+        }else{
+            $this->obj->invokeArgs($name,[$args]);
+        }
     }
 }
