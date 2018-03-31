113407f513ecafba885c653d15059bab4e766cbf
siburuxue <siburuxue@sina.com>
2018-03-31 13:12:05 +0800

    修改为工厂模式

--- test.php
+++ test.php
@@ -3,5 +3,5 @@ require('Factory.php');
 use ZP\Factory;
 $type = $_GET['type'];
 $factory = new Factory($type);
-var_dump($factory->create());
+var_dump($factory->create());exit;
 $factory->create()->send();
