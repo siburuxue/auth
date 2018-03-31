7bcfb9a3bf0e04d3ee2115991ea306fab7a269ee
siburuxue <siburuxue@sina.com>
2018-03-31 11:41:32 +0800

    修改为工厂模式

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
         'app_id' => "d76c6418b1fe6fb7a67e",
         'app_secret' => "558fed46acf44438592ba711a03d487edfbcd794",
         'auth_url' => 'https://github.com/login/oauth/authorize',
-        'auth_callback' => 'http://auth.siburuxue.com/github_auth_callback.php',
+        'auth_callback' => 'http://auth.siburuxue.com/auth_callback.php',
         'token_url' => 'https://github.com/login/oauth/access_token',
         'user_url' => 'https://api.github.com/user?access_token=',
     ],
