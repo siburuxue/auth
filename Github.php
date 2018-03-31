18c5d9ffc327202e3b3322d68da1523be11b3840
siburuxue <siburuxue@sina.com>
2018-03-31 13:48:07 +0800

    修改为工厂模式

--- Github.php
+++ Github.php
@@ -16,7 +16,7 @@ class Github extends Auth
 
     public function getInfo($args)
     {
-        echo json_encode($args);exit;
+        $args = $args[0];
         $code = $args['code'];
         $url = $this->config['Github']['token_url']."?client_id=".$this->config['Github']['app_id']."&client_secret=".$this->config['Github']['app_secret']."&code=".$code."&redirect_uri=".urlencode($this->config['Github']['auth_callback'])."&state=Github";
         $header = ['Accept:application/json'];
